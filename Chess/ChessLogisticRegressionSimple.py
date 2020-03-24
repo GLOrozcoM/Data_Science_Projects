c11245d10d8fcca8f6bf721421f0127387105bf6
Leonard Orozco <glo003@bucknell.edu>
2020-03-24 10:19:55 -0700

    Logistic regression: Added in a classification report. Encountered that aweful reshape error. Normalized variables.

--- Chess/ChessLogisticRegressionSimple.py
+++ Chess/ChessLogisticRegressionSimple.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import normalize
 
 def run_kfold(dataset, predictors, response, model, splits):
-    predictor = dataset[predictors]
+    predictor = normalize(dataset[predictors])
     responses = dataset[response]
-    kscore = cross_val_score(model, predictor, responses, cv=splits).sum() / splits
+    kscore = cross_val_score(model, predictor, responses, cv = splits).sum() / splits
     print("Kfold " + str(splits) + " score: ", kscore)
 
 def main():
@@ -16,17 +19,37 @@ def main():
     response = 'higher_rating_won'
     lg = LogisticRegression()
 
-    print("Scores for single predictor logistic regression:")
     FIRST_SPLIT = 5
-    run_kfold(games, predictor, response, lg, FIRST_SPLIT)
-
     SECOND_SPLIT = 10
+
+    print("K scores for single predictor logistic regression:")
+    run_kfold(games, predictor, response, lg, FIRST_SPLIT)
     run_kfold(games, predictor, response, lg, SECOND_SPLIT)
 
-    print("Scores for multiple logistic regression:")
+
+    X_train, x_test, y_train, y_test = train_test_split(games.abs_diff_rating,
+                                                        games.higher_rating_won,
+                                                        test_size = 0.33, random_state = 1)
+    lg.fit(X_train.values.reshape(-1,1), y_train.values.ravel())
+    predictions = lg.predict(x_test.values.reshape(-1,1))
+    report = classification_report(predictions, y_test)
+    print("Classification report: ")
+    print(report)
+
+    print("K scores for multiple logistic regression:")
     predictors = ['abs_diff_rating', 'higher_rating_coded', 'turns', 'white_higher_rated']
     run_kfold(games, predictors, response, lg, FIRST_SPLIT)
     run_kfold(games, predictors, response, lg, SECOND_SPLIT)
+
+    X_train, x_test, y_train, y_test = train_test_split(normalize(games[predictors]),
+                                                        games.higher_rating_won,
+                                                        test_size=0.33, random_state=1)
+    lg.fit(X_train, y_train)
+    predictions = lg.predict(x_test)
+    report = classification_report(predictions, y_test)
+    print("Classification report: ")
+    print(report)
+
 
     print('Main completed')
     return 0
